6f3d63a766b053dddd2369c03c54eb20e2b2cab2
Elias Dobrin <elias@dobrin.net>
2023-03-15 11:27:11 -0700

    More development on softquake library file.

--- softquake/softquake.py
+++ softquake/softquake.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from math import tau, sin
+from math import tau, sin, cos
 from softbodies import Softbody, Node, Link
 from vectors import Vector
 
@@ -9,15 +9,8 @@ class Sine:
     amplitude: float
     phase: float
 
-    def get_value(self, time: float) -> float:
+    def get_sine(self, time: float) -> float:
         return self.amplitude * sin(tau * (self.frequency * time - self.phase))
-
-
-class Signal:
-    sines: list[Sine]
-
-    def get_value(self, time: float) -> float:
-        return sum(sine.get_value(time) for sine in self.sines)
 
 
 class Plate:
@@ -26,7 +19,16 @@ class Plate:
     velocity: Vector
     acceleration: Vector
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sines: list[Sine], time: float) -> None:
+        self.sines = sines
+        self.set_kinetics(time)
 
-    def set_values(self, time: float) -> :
+    def set_kinematics(self, time: float) -> None:
+        self.position.x = 0
+        self.velocity.x = 0
+        self.acceleration.x = 0
+
+        for sine in self.sines:
+            self.position.x += sine.amplitude * sin(tau * (sine.frequency * time + sine.phase))
+            self.velocity.x += tau * sine.frequency * sine.amplitude * cos(tau * (sine.frequency * time + sine.phase))
+            self.acceleration.x -= (tau * sine.frequency) ** 2 * sine.amplitude * sin(tau * (sine.frequency * time + sine.phase))
