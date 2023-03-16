07794e6b2dcae1a4811cf40f1f887a65c79f025a
Elias Dobrin <elias@dobrin.net>
2023-03-15 20:21:14 -0700

    More progress on the rendering workflow.

--- softquake/main.py
+++ softquake/main.py
@@ -1,4 +1,6 @@
-from os import path, mkdir, rmdir
+from math import tau
+from os import path, mkdir, rmdir, remove
+from glob import glob
 import cairo
 import ffmpeg
 import matplotlib.pyplot as plt
@@ -11,10 +13,35 @@ if not path.exists("snapshots"):
 
 fps = 60
 ips = 100
+delta = 1 / (fps * ips)
 time = 0
 shot = 0
 
-plate = RigidPlate(sines=[Sine(frequency=1, amplitude=0.2)], width=5, nodes=[])
+plate = RigidPlate(sines=[Sine(frequency=1, amplitude=0.2)], width=4, nodes=[])
+
+nodes = []
+
+for x in range(4):
+    for y in range(8):
+        node = Node(mass=1, position=Vector(3 * (x / 3 - 0.5), 6 * y / 7).add(plate.position))
+        nodes.append(node)
+
+links = []
+
+for x in range(3):
+    for y in range(8):
+        link = Link(nodes=(nodes[8 * x + y], nodes[8 * (x + 1) + y]), stiffness=5000, dampening=20)
+        links.append(link)
+
+for x in range(4):
+    for y in range(7):
+        link = Link(nodes=(nodes[8 * x + y], nodes[8 * x + (y + 1)]), stiffness=5000, dampening=20)
+        links.append(link)
+
+for x in range(3):
+    for y in range(7):
+        link = Link(nodes=(nodes[8 * x + y], nodes[8 * (x + 1) + (y + 1)]), stiffness=5000, dampening=20)
+        links.append(link)
 
 for t in range(10):
     for s in range(fps):
@@ -22,7 +49,17 @@ for t in range(10):
             plate.set_kinematics(time)
             plate.set_nodes()
 
-            time += 1 / (fps * ips)
+            for node in nodes:
+                node.force.set(Vector(0, -9.8 * node.mass))
+
+            for node in nodes:
+                if node not in plate.nodes:
+                    acceleration = node.acceleration.copy()
+                    node.acceleration = node.force / node.mass
+                    node.position += node.velocity * delta + 0.5 * node.acceleration * delta ** 2
+                    node.velocity += 0.5 * (acceleration + node.acceleration) * delta
+
+            time = delta * (t * fps + s * ips + i)
 
         surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1000)
         context = cairo.Context(surface)
@@ -50,8 +87,28 @@ for t in range(10):
         context.set_source_rgb(0, 0, 0)
         context.stroke()
 
+        for link in links:
+            context.move_to(link.nodes[0].position.x, link.nodes[0].position.y)
+            context.line_to(link.nodes[1].position.x, link.nodes[1].position.y)
+            context.set_line_width(0.1)
+            context.set_source_rgb(0, 0, 0)
+            context.stroke()
 
-        surface.write_to_png(f"snapshots/{shot}.png")
+        for node in nodes:
+            context.arc(node.position.x, node.position.y, 0.15, 0, tau)
+            context.set_source_rgb(1, 1, 1)
+            context.fill_preserve()
+            context.set_line_width(0.1)
+            context.set_source_rgb(0, 0, 0)
+            context.stroke()
+
+        surface.write_to_png(f"snapshots/{shot:05}.png")
         shot += 1
-        break
-    break
+
+ffmpeg.input("snapshots/%05d.png", framerate=60).output("softquake.mp4").run(overwrite_output=True, quiet=False)
+
+files = glob("snapshots/*")
+for file in files:
+    remove(file)
+
+rmdir("snapshots")
