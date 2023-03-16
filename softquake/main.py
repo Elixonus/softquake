adcfed6e9c536a7c097e60af791d606e1e73cad6
Elias Dobrin <elias@dobrin.net>
2023-03-15 18:34:29 -0700

    More progress on the rendering workflow.

--- softquake/main.py
+++ softquake/main.py
@@ -12,8 +12,9 @@ if not path.exists("snapshots"):
 fps = 60
 ips = 100
 time = 0
+shot = 0
 
-plate = RigidPlate(sines=[Sine(frequency=1, amplitude=0.2)], width=1, nodes=[])
+plate = RigidPlate(sines=[Sine(frequency=1, amplitude=0.2)], width=5, nodes=[])
 
 for t in range(10):
     for s in range(fps):
@@ -23,3 +24,34 @@ for t in range(10):
 
             time += 1 / (fps * ips)
 
+        surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1000)
+        context = cairo.Context(surface)
+
+        context.translate(0, 500)
+        context.scale(1, -1)
+        context.translate(0, -500)
+
+        context.rectangle(0, 0, 1000, 1000)
+        context.set_source_rgb(0.5, 1, 1)
+        context.fill()
+
+        context.rectangle(0, 0, 1000, 200)
+        context.set_source_rgb(0, 1, 0)
+        context.fill()
+
+        context.scale(100, 100)
+        context.translate(5, 5)
+        context.translate(0, -4)
+
+        context.rectangle(plate.position.x - 0.5 * plate.width, plate.position.y - 0.5, plate.width, 0.5)
+        context.set_source_rgb(0.5, 0.25, 0.125)
+        context.fill_preserve()
+        context.set_line_width(0.1)
+        context.set_source_rgb(0, 0, 0)
+        context.stroke()
+
+
+        surface.write_to_png(f"snapshots/{shot}.png")
+        shot += 1
+        break
+    break
